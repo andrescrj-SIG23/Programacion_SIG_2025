c0b8b94aff3fe2b0c3cc0f9e65ed2574d3b62fbc
Jorge <lizpe0102@gmail.com>
2025-08-04 21:40:00 -0500

    0.2

---
+++ docs_1/programa.js
@@ -0,0 +1,12 @@
+ <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
+     integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
+     crossorigin=""></script>
+
+     var map = L.map('map').setView([4.628610867121586, -74.0659232651548], 13);
+
+     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+var marker = L.marker([4.628610867121586, -74.0659232651548]).addTo(map);
